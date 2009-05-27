842afba4285c712b8762661b99401a44edb53c78
Evan Klitzke <evan@eklitzke.org>
2009-05-26 18:05:05 -0700

    make it mostly work

--- inotifyreload.py
+++ inotifyreload.py
@@ -4,12 +4,20 @@ import signal
 import sys
 import threading
 import time
+import types
 
 import pyinotify
 
 def graceful_apache():
 	"Sends a graceful signal to Apache"
+	log('Sending graceful signal')
 	os.kill(os.getpid(), signal.SIGUSR1)
+
+def log(msg):
+	import datetime
+	msg = '[%s pid=%d] %s\n' % (datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), os.getpid(), msg)
+	f = open('/nail/home/evan/inotify.log', 'a')
+	f.write(msg)
 
 class ReloadThread(threading.Thread):
 	"""Bump Apache when open .py files change; uses inotify for efficiency.
@@ -25,14 +33,14 @@ class ReloadThread(threading.Thread):
 
 	def __init__(self, prefix, interval=1.0):
 		threading.Thread.__init__(self)
-		self.prefix = prefix
+		self.prefix = os.path.realpath(prefix)
 		self.interval = interval
 		self.known_files = set()
 
-		def kill_self(fname):
-			if fname in self.known_files:
-				f = open('/nail/home/evan/inotify.log', 'a')
-				f.write('gracefuling, pid = %d\n' % os.getpid())
+		def kill_self(inotify_obj):
+			#log('got notification for %s' % inotify_obj)
+			pathname = self.canonicalize_filename(inotify_obj.pathname)
+			if pathname in self.known_files:
 				graceful_apache()
 
 		self.wm = pyinotify.WatchManager()
@@ -41,6 +49,12 @@ class ReloadThread(threading.Thread):
 		self.setDaemon(True)
 		self.daemon = True
 
+	@staticmethod
+	def canonicalize_filename(filename):
+		if filename.endswith('.pyc') or filename.endswith('.pyo'):
+			filename = filename[:-1]
+		return os.path.realpath(filename)
+
 	def update_known_files(self):
 		"""Update self.known_files
 
@@ -48,41 +62,30 @@ class ReloadThread(threading.Thread):
 		maintains a list of these files in self.known_files.
 		"""
 
-		def canonicalize_filename(filename):
-			if filename.endswith('.pyc') or filename.endswith('.pyo'):
-				return filename[:-1]
-			return filename
-
-		loaded_files = (getattr(x, '__file__', None) for x in sys.modules.values())
-		loaded_files = (x for x in loaded_files if x and x.startswith(self.prefix))
-		self.known_files = set(canonicalize_filename(x) for x in loaded_files)
+		loaded_files = (getattr(x, '__file__', None) for x in sys.modules.values() if type(x) is types.ModuleType)
+		loaded_files = (self.canonicalize_filename(x) for x in loaded_files if x is not None)
+		loaded_files = (x for x in loaded_files if x.startswith(self.prefix))
+		self.known_files = set(loaded_files)
 
 	def run(self):
-		try:
-			f = open('/nail/home/evan/inotify.log', 'a')
-			f.write('Running, pid = %d\n' % os.getpid())
-			f.close()
-			self.notifier.start()
+		self.notifier.start()
 
-			mask = (pyinotify.IN_CREATE |
-					pyinotify.IN_DELETE |
-					pyinotify.IN_MODIFY )
+		mask = (pyinotify.IN_CREATE |
+				pyinotify.IN_DELETE |
+				pyinotify.IN_MODIFY )
 
+		self.update_known_files()
+		self.wm.add_watch(self.prefix, mask, rec=True)
+
+		# Spin forever, updating the list of known open modules every
+		# self.interval seconds
+		while True:
+			time.sleep(self.interval)
 			self.update_known_files()
-			self.wm.add_watch(self.prefix, mask, rec=True)
-
-			# Spin forever, updating the list of known open modules every
-			# self.interval seconds
-			while True:
-				time.sleep(self.interval)
-				self.update_known_files()
-		except Exception:
-			print 'fuck'
 
 def start():
-	print 'starting'
 	username = pwd.getpwuid(os.getuid()).pw_name
-	pgname = '/nail/pg/%s/loc' % username
+	pgname = os.readlink('/nail/pg/%s/loc' % username)
 	reload_thread = ReloadThread(prefix=pgname)
 	reload_thread.start()
 
